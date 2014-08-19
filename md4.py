e234e3678cfe3a490879c97dcae7c161e17e37ad
Robert Picard <mail@robert.io>
2014-08-19 15:19:58 -0700

    Well, it's all there. It's probably wrong in a lot of ways, but it's there.

--- md4.py
+++ md4.py
@@ -25,11 +25,11 @@ def md4(message):
     message += [ord(c) for c in pack('>Q', original_bit_length & 0xFFFFFFFFFFFFFFFF)]
 
     # initialize the registers to magic values
-    # TODO I think these are the wrong endian
-    A = 0x01234567
-    B = 0x89abcdef
-    C = 0xfedcba98
-    D = 0x76543210
+    # TODO these could be the wrong endian
+    A = 0x67452301
+    B = 0xefcdab89
+    C = 0x98badcfe
+    D = 0x10325476
 
     # define F, G, and H
 
@@ -55,72 +55,84 @@ def md4(message):
         DD = D
 
         # round 1
-        r1 = lambda a,b,c,d,k,s: rl((a + F(b,c,d) + X[k]), s)
+        FF = lambda a,b,c,d,k,s: rl((a + F(b,c,d) + X[k]), s)
 
         # perform the 16 operations
-        r1(a,b,c,d,0,3)
-        r1(d,a,b,c,1,7)
-        r1(c,d,a,b,2,11)
-        r1(b,c,d,a,3,19)
+        A = FF(A,B,C,D,0,3)
+        D = FF(D,A,B,C,1,7)
+        C = FF(C,D,A,B,2,11)
+        B = FF(B,C,D,A,3,19)
 
-        r1(a,b,c,d,4,3)
-        r1(d,a,b,c,5,7)
-        r1(c,d,a,b,6,11)
-        r1(b,c,d,a,7,19)
+        A = FF(A,B,C,D,4,3)
+        D = FF(D,A,B,C,5,7)
+        C = FF(C,D,A,B,6,11)
+        B = FF(B,C,D,A,7,19)
 
-        r1(a,b,c,d,8,3)
-        r1(d,a,b,c,9,7)
-        r1(c,d,a,b,10,11)
-        r1(b,c,d,a,11,19)
+        A = FF(A,B,C,D,8,3)
+        D = FF(D,A,B,C,9,7)
+        C = FF(C,D,A,B,10,11)
+        B = FF(B,C,D,A,11,19)
 
-        r1(a,b,c,d,12,3)
-        r1(d,a,b,c,13,7)
-        r1(c,d,a,b,14,11)
-        r1(b,c,d,a,15,19)
+        A = FF(A,B,C,D,12,3)
+        D = FF(D,A,B,C,13,7)
+        C = FF(C,D,A,B,14,11)
+        B = FF(B,C,D,A,15,19)
 
         # round 2
-        r2 = lambda a,b,c,d,k,s: rl((a + G(b,c,d) + X[k] + 0x5A827999), s)
+        GG = lambda a,b,c,d,k,s: rl((a + G(b,c,d) + X[k] + 0x5A827999), s)
 
         # perform the 16 operations
-        r2(a,b,c,d,0,3)
-        r2(d,a,b,c,4,5)
-        r2(c,d,a,b,8,9)
-        r2(b,c,d,a,12,13)
+        A = GG(A,B,C,D,0,3)
+        D = GG(D,A,B,C,4,5)
+        C = GG(C,D,A,B,8,9)
+        B = GG(B,C,D,A,12,13)
 
-        r2(a,b,c,d,1,3)
-        r2(d,a,b,c,5,5)
-        r2(c,d,a,b,9,9)
-        r2(b,c,d,a,13,13)
+        A = GG(A,B,C,D,1,3)
+        D = GG(D,A,B,C,5,5)
+        C = GG(C,D,A,B,9,9)
+        B = GG(B,C,D,A,13,13)
 
-        r2(a,b,c,d,2,3)
-        r2(d,a,b,c,6,5)
-        r2(c,d,a,b,10,9)
-        r2(b,c,d,a,14,13)
+        A = GG(A,B,C,D,2,3)
+        D = GG(D,A,B,C,6,5)
+        C = GG(C,D,A,B,10,9)
+        B = GG(B,C,D,A,14,13)
 
-        r2(a,b,c,d,3,3)
-        r2(d,a,b,c,7,5)
-        r2(c,d,a,b,11,9)
-        r2(b,c,d,a,15,13)
+        A = GG(A,B,C,D,3,3)
+        D = GG(D,A,B,C,7,5)
+        C = GG(C,D,A,B,11,9)
+        B = GG(B,C,D,A,15,13)
 
         # round 3
-        r3 = lambda a,b,c,d,k,s: rl((a + H(b,c,d) + X[k] + 0x6ED9EBA1), s)
+        HH = lambda a,b,c,d,k,s: rl((a + H(b,c,d) + X[k] + 0x6ED9EBA1), s)
 
-        r3(a,b,c,d,0,3)
-        r3(d,a,b,c,8,9)
-        r3(c,d,a,b,4,11)
-        r3(b,c,d,a,12,15)
+        A = HH(A,B,C,D,0,3)
+        D = HH(D,A,B,C,8,9)
+        C = HH(C,D,A,B,4,11)
+        B = HH(B,C,D,A,12,15)
 
-        r3(a,b,c,d,2,3)
-        r3(d,a,b,c,10,9)
-        r3(c,d,a,b,6,11)
-        r3(b,c,d,a,14,15)
+        A = HH(A,B,C,D,2,3)
+        D = HH(D,A,B,C,10,9)
+        C = HH(C,D,A,B,6,11)
+        B = HH(B,C,D,A,14,15)
 
-        r3(a,b,c,d,1,3)
-        r3(d,a,b,c,10,9)
-        r3(c,d,a,b,6,11)
-        r3(b,c,d,a,13,15)
+        A = HH(A,B,C,D,1,3)
+        D = HH(D,A,B,C,10,9)
+        C = HH(C,D,A,B,6,11)
+        B = HH(B,C,D,A,13,15)
 
-        r3(a,b,c,d,3,3)
-        r3(d,a,b,c,11,9)
-        r3(c,d,a,b,7,11)
-        r3(b,c,d,a15,15)
+        A = HH(A,B,C,D,3,3)
+        D = HH(D,A,B,C,11,9)
+        C = HH(C,D,A,B,7,11)
+        B = HH(B,C,D,A,15,15)
+
+       # increment by previous values
+       A += AA 
+       B += BB
+       C += CC
+       D += DD
+
+    digest = (A << 32) | B
+    digest = (digest << 32) | C
+    digest = (digest << 32) | D
+
+    return digest
