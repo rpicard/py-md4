ae2aed78a0984b8344280ce39a56a8dee1de69d7
Robert Picard <mail@robert.io>
2014-08-20 11:51:34 -0700

    Figured out how to represent. Also figured out padding

--- md4.py
+++ md4.py
@@ -6,31 +6,21 @@ def md4(message):
     """
 
     # we'll need to remember this for later
-    original_byte_length = len(message)
-    original_bit_length = original_byte_length * 8
+    original_length = len(message)
 
-    # turn message into a list of 32-bit words
-    m = [ord(c) for c in message]
-    M = []
-    for i in xrange(0, len(m) - 5, 4):
-        index = i/4
-        M.append(m[i])
-        M[index] = (M[index] << 8) | m[i+1]
-        M[index] = (M[index] << 8) | m[i+2]
-        M[index] = (M[index] << 8) | m[i+3]
-        
-    # add a '1' bit (how the hell is 0x80 a '1' bit?)
-    # DO IT HOW?
-    M += [0x80]
+    message = [ord(c) for c in message]
 
-    # add <= 511 '0' bits until the length is congruent to 448 % 512
-    for i in xrange(512):
-        if (original_byte_length + i) % 512 == 448:
-            for j in xrange(i):
-                M += [0x00]
+    # add a '1' bit via a byte
+    message += [0x80]
+
+    # padding to 448 % 512 bits (56 % 64 byte)
+    if len(message) < 56:
+        message += [0x00] * (56 - len(message))
+    else:
+        message += [0x00] * (120 - len(message))
 
     # add the length as a 64 bit big endian, use lower order bits if length overflows 2^64
-    M += [ord(c) for c in pack('>Q', original_bit_length & 0xFFFFFFFFFFFFFFFF)]
+    message += [ord(c) for c in pack('>Q', (original_length * 8) & 0xFFFFFFFFFFFFFFFF)]
 
     # initialize the registers to magic values
     # TODO these could be the wrong endian
@@ -40,17 +30,31 @@ def md4(message):
     D = 0x10325476
 
     # define F, G, and H
-
     F = lambda x,y,z: (x & y) | ((~x) & z)
     G = lambda x,y,z: (x & y) | (x & z) | (y & z)
     H = lambda x,y,z: x ^ y ^ z
 
+    # round functions
+    FF = lambda a,b,c,d,k,s: rl((a + F(b,c,d) + X[k]), s)
+    GG = lambda a,b,c,d,k,s: rl((a + G(b,c,d) + X[k] + 0x5A827999), s)
+    HH = lambda a,b,c,d,k,s: rl((a + H(b,c,d) + X[k] + 0x6ED9EBA1), s)
+
     # define a 32-bit left-rotate function (<<< in the RFC)
     rl = lambda x, n: (x << n) | ((x & 0xFFFFFFFF) >> (32-n))
 
+    # turn the padded message into a list of 32-bit words
+    M = []
+    for i in xrange(0, len(message) - 5, 4):
+
+        index = i/4
+        M.append(message[i])
+        M[index] = (M[index] << 8) | message[i+1]
+        M[index] = (M[index] << 8) | message[i+2]
+        M[index] = (M[index] << 8) | message[i+3]
+        
+
     # process each 16 word (64 byte) block
     for i in xrange(0, len(M) - 16, 16):
-        # TODO will this get the last byte?
 
         X = M[i:i+16]
 
@@ -61,7 +65,6 @@ def md4(message):
         DD = D
 
         # round 1
-        FF = lambda a,b,c,d,k,s: rl((a + F(b,c,d) + X[k]), s)
 
         # perform the 16 operations
         A = FF(A,B,C,D,0,3)
@@ -85,7 +88,6 @@ def md4(message):
         B = FF(B,C,D,A,15,19)
 
         # round 2
-        GG = lambda a,b,c,d,k,s: rl((a + G(b,c,d) + X[k] + 0x5A827999), s)
 
         # perform the 16 operations
         A = GG(A,B,C,D,0,3)
@@ -109,7 +111,6 @@ def md4(message):
         B = GG(B,C,D,A,15,13)
 
         # round 3
-        HH = lambda a,b,c,d,k,s: rl((a + H(b,c,d) + X[k] + 0x6ED9EBA1), s)
 
         A = HH(A,B,C,D,0,3)
         D = HH(D,A,B,C,8,9)
@@ -132,10 +133,10 @@ def md4(message):
         B = HH(B,C,D,A,15,15)
 
         # increment by previous values
-        A =  ((A + AA) & 0xFFFFFFFF) + 0x1000000000
-        B =  ((B + BB) & 0xFFFFFFFF) + 0x1000000000
-        C =  ((C + CC) & 0xFFFFFFFF) + 0x1000000000
-        D =  ((D + DD) & 0xFFFFFFFF) + 0x1000000000
+        A =  ((A + AA) & 0xFFFFFFFF)
+        B =  ((B + BB) & 0xFFFFFFFF)
+        C =  ((C + CC) & 0xFFFFFFFF)
+        D =  ((D + DD) & 0xFFFFFFFF)
 
     digest = (A << 32) | B
     digest = (digest << 32) | C
